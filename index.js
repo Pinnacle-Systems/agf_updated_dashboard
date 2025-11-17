c00cf055bbe212140c16cb4bb0f6d247420592f1
karthick <karthickcs10124@gmail.com>
2025-11-17 10:10:05 +0530

    git changes4

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const require = createRequire(import.meta.url);
 const oracledb = require('oracledb');
 
 
-console.log("oracledb 7");
+console.log("oracledb 9");
 
 import { socketMain } from "./src/sockets/socket.js";
 
